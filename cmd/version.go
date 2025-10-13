5a07917067d5cecffc0f4af94948a3e959fbe4f8
lutos5 <86042308+vonaxs@users.noreply.github.com>
2025-10-13 14:37:59 +0800

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 var (
 	version  = "0.9.5"
 	codename = "XrayR"
-	intro    = "A Xray backend that supports many panels"
+	intro    = "A Xray backend that supports many panels(vision fixed)"
 )
 
 func init() {
